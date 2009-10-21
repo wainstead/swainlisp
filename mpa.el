1965d7075289882596d99b828fb786295acb3cb0
swain <swain@85e75c80-f03f-0410-9e47-ffaef1773115>
2009-10-21 14:34:40 +0000

    updating mpa-user, stripping out some old useless stuff and adding service_id; and adding its clone, plr-user. I'm lazy and copied and pasted the code. I'll refactor it later, mom, I promise.
    git-svn-id: https://svn.myphotoalbum.com/swainlisp/trunk@50 85e75c80-f03f-0410-9e47-ffaef1773115

--- mpa.el
+++ mpa.el
@@ -77,7 +77,15 @@ WHERE
   (switch-to-buffer "sql")
   (goto-char (point-max))
   (interactive "sUsername: ")
-  (insert (format "select * from users where username='%s'\\G select concat('http://', username, '.%s/') as link from users where username='%s'; select concat('http://www.%s/activate.php?u=', username, '&uid=', userid) as activation_link from users where username='%s'; select concat('http://', username, '.%s/ampira.redirect.php?uname=', username, '&password=', password) as login_link from users where username='%s';" user sw-my-hostname user sw-my-hostname user sw-my-hostname user))
+  (insert (format "select * from users where username='%s' and service_id=1\\G select concat('http://', username, '.%s/') as link from users where service_id=2 and username='%s'; " user  sw-my-hostname user))
+  (comint-send-input))
+
+(defun plr-user (user)
+  "Look up an mpa user by username."
+  (switch-to-buffer "sql")
+  (goto-char (point-max))
+  (interactive "sUsername: ")
+  (insert (format "select * from users where username='%s' and service_id=2\\G select concat('http://', username, '.%s/') as link from users where service_id=2 and username='%s'; " user  sw-my-hostname user))
   (comint-send-input))
 
 
