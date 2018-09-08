66d56570ba64524102a103e4f4dd0dd26b7a4240
Steve Wainstead <swain@abzug.attlocal.net>
2018-09-08 11:08:04 -0400

    Reorganizing my lisp files
    Externally acquired files: external-packages
    Emacs customization files: cusomization-files
    .emacs files from various machines: dotemacs-files
    Lisp I wrote: personal-lisp

---
+++ personal-lisp/desktop-auto-save.el
@@ -0,0 +1,213 @@
+;; Copyright 2005 Steve Wainstead (swain@panix.com)
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+
+;; ABOUT:
+
+;; desktop-auto-save extends the functionality of Emacs's feature of
+;; saving your "desktop" (that is, the list of files you have
+;; open). It adds the feature of saving the contents of shell buffers
+;; to files whenever the desktop is saved.
+
+;; Additionally if you store all the files in a directory and turn
+;; that directory into a git repository, desktop-auto-save commits
+;; those files when you quit Emacs.
+
+;; put these in your .emacs file or similar.
+
+;; ;; load the desktop on startup (from ~/)
+;; (desktop-load-default)
+;; ;; automatically save the desktop on exit.
+;; (setq desktop-enable t)
+;; (load-file "~swain/.elisp/desktop-auto-save.el")
+
+
+
+;; save the desktop every time you auto-save-file
+(defun desktop-auto-save ()
+  "Added to auto-save-hook so the desktop is not lost."
+  (desktop-save "~/")
+  (sw-save-shell-buffer-contents)
+  ;;(message "Wrote desktop.")
+  )
+
+(add-hook 'auto-save-hook 'desktop-auto-save t)
+
+
+
+(defvar sw-restore-shell-buffers-flag t
+  "If t, shell buffer contents are inserted from the previous session,
+if available.  (This is the default). If nil, previous shell
+buffer contents are not restored.  On some systems (or some
+versions of systems? Or Emacs?) the shell tries to execute the
+contents of the file after insertion, with disasterous
+consequences.")
+
+
+;; For all shell buffers, save their contents to individual files. The
+;; idea here is to preserve their contents in case of a crash.
+(defun sw-save-shell-buffer-contents ()
+  "Find and save all buffers that are in shell-mode"
+  (interactive)
+  (and (file-accessible-directory-p sw-buffer-file-name-prefix)
+       (let ( (bufflist (buffer-list)) ) 
+         (while bufflist
+           (setq buff (car bufflist))
+           (save-excursion
+             (set-buffer buff)
+             (when (or (string= major-mode "shell-mode") (or (string= (buffer-name) "psql") (string= (buffer-name) "*Python*")) )
+               ;;(message (format "it's a shell: %s" (buffer-name buff)))
+               ;; if this buffer's contents have not been reloaded...
+
+               ;; thus far, this is just not working out... I want a
+               ;; buffer-local variable that indicates whether the buffer
+               ;; contents have been inserted. I want to insert them if
+               ;; it's nil. What's happening is the variable is "void" and
+               ;; desktop-write goes into an infinite loop making system
+               ;; beeps.
+
+               ;;(if (and (boundp 'buffer-contents-restored) (not buffer-contents-restored)) (sw-insert-saved-buffer-contents (buffer-name buff)))
+               (sw-save-buffer-invisibly buff)
+               )
+             (setq bufflist (cdr bufflist))
+             )
+           )
+         )
+       )
+  )
+
+(defvar sw-buffer-file-name-prefix "~swain/.emacs.shellbuffers/"
+  "All shell buffers, when their contents are saved, get this string prefixed to the buffer name.")
+
+;; save a shell buffer's contents to ~/.emacs.shellbuffers/buffer-name; doesn't switch
+;; buffer on user; hack from desktop.el which showed how to use write-region
+;; to save the contents invisibly
+(defun sw-save-buffer-invisibly (buffer)
+  "Write out shell buffer's contents for preservation behind-the-scenes."
+  (set-buffer buffer)
+  (let ( (coding-system-for-write 'no-conversion) ) 
+    ;; (write-region (point-min) (point-max) (concat sw-buffer-file-name-prefix (buffer-name buffer) (format-time-string "-%Y-%m-%d"))))
+    (write-region (point-min) (point-max) (sw-make-buffer-filename buffer)))
+  )
+
+(defun sw-make-buffer-filename (buffer)
+  "Make a buffer file name string from the buffer name passed in
+by prefixing the string passed in with the contents of the
+variable sw-buffer-file-name-prefix. For example: pass in 'cli'
+and it returns '~swain/.emacs.shellbuffers/cli'"
+  ;;(message (concat sw-buffer-file-name-prefix buffname))
+  (concat sw-buffer-file-name-prefix (buffer-name buffer))
+  )
+
+;; so far, no go (no va) FIXME
+(defun sw-advance-point-log-buffers ()
+  "Saving a window config to register also preserves the location of point, which we 
+   don't want. Cycle through the log file buffers and send point to point-max."
+  (interactive) 
+  (let ( (file-alist sw-tail-file-alist) )
+    (while (consp file-alist)
+      (setq pair (car file-alist))
+      (set-buffer (car pair))
+      (message (format "in buffer %s" (car pair)))
+      (end-of-buffer)
+      (insert "\ntest message\n")
+      (setq file-alist (cdr file-alist))
+      )
+    )
+  )
+
+
+;; The idea here is to "restore" a shell buffer's contents. For
+;; example: you have a shell buffer named "foo." The contents of the
+;; shell buffer "foo" should be saved when you quit Emacs (saving the
+;; contents of all shell buffers is the whole point of this Emacs Lisp
+;; file). When you again create a new shell buffer named "foo" the
+;; contents of the previous incarnation of "foo" should be loaded into
+;; the "foo" shell buffer at the top. Unfortunately it just inserts
+;; the text from the previous shell buffer; a future hack will be to
+;; insert not the text, but the actual Emacs lisp code (see
+;; "buffer-string") representing the shell buffer as it existed
+;; previously, including the command history.
+(defun sw-insert-saved-buffer-contents (sw-buff-name)
+  (message "Inserting auto-desktop-save data...")
+  (goto-char (point-min))
+  (insert-file (concat sw-buffer-file-name-prefix sw-buff-name))
+  (goto-char (point-max))
+  ;;(setq buffer-contents-restored t) ;; should be buffer local variable, first created in sw-shell
+  )
+
+
+;; Rather than using version control like git or svn, rename the shell
+;; buffer contents file with a date (currently unused)
+(defun sw-backup-saved-buffer-contents (sw-buff-name)
+  "Make a backup copy of the shell buffer auto save file"
+  (message (format "Backing up %s shell buffer contents..." sw-buff-name))
+  (rename-file 
+   (concat sw-buffer-file-name-prefix sw-buff-name)
+   (concat sw-buffer-file-name-prefix sw-buff-name "." (format-time-string "-%Y-%m-%d")))
+)
+
+
+;; deprecated
+;; As of april 2009, here's the way to do it; relies on our ability to
+;; svn revert the shellbuffer files. You have "within the hour" to
+;; restore the shell buffer contents before the cron job commits them
+;; to the local svn repository. After that, you'll have to manually
+;; retrieve the version you want.
+;; (defun sw-restore-shellbuffers-contents ()
+;;   "do an svn revert on the shell buffers directory, then for each
+;; open buffer, insert its file at point-min"
+;;   (interactive)
+;;   (shell-command "svn revert ~swain/.emacs.shellbuffers/*")
+;;   ;; now recurse, since they are reverted. Don't want to revert every
+;;   ;; time.
+;;   (sw-insert-previous-shellbuffer-contents (list "cli" "root" "www" "sql")))
+
+;; deprecated
+;; (defun sw-restore-recent-shellbuffers-contents ()
+;;   "Don't do an svn revert on the shell buffers directory, then for each
+;; open buffer, insert its file at point-min"
+;;   (interactive)
+;;   (sw-insert-previous-shellbuffer-contents (list "cli" "root" "www" "sql")))
+
+;; recursive function called by sw-restore-shellbuffers-contents
+(defun sw-insert-previous-shellbuffer-contents (shell-buffer-list)
+  "insert shell buffer contents from a previous session, and
+   recurse on the list of shellbuffer names to do the same for each
+   shell buffer so named."
+  (if (car shell-buffer-list)
+      (progn
+        (switch-to-buffer (get-buffer (car shell-buffer-list)))
+        (goto-char (point-min))
+        (insert-file (format "~swain/.emacs.shellbuffers/%s" (car shell-buffer-list)))
+        (goto-char (point-max))
+        (sw-insert-previous-shellbuffer-contents (cdr shell-buffer-list))
+        )
+    )
+  )
+
+(defun sw-git-commit-buffers()
+  "Save the contents of all shell buffers to their files and then
+   git-commit those files in ~/.emacs.shellbuffers."
+  (interactive)
+  (sw-save-shell-buffer-contents)
+  (shell-command "cd ~swain/.emacs.shellbuffers; git commit -am \"Committing buffers\"")
+)
+;; when quiting Emacs save and commit the shell buffers
+(add-hook 'kill-emacs-hook 'sw-git-commit-buffers t)
+
---
+++ personal-lisp/python-settings.el
@@ -0,0 +1,98 @@
+;;; package -- Summary
+
+;;; Commentary:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; originally from swainlib.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; python mode stuff, if I have it
+;; (load "python-mode")
+;; (setq auto-mode-alist
+;;       (append '(
+;;                 ("\\.py\\'" . python-mode)) auto-mode-alist))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; originally from dotemacs.nfmc
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; load a newer version of python-mode
+;; (add-to-list 'load-path "~/toolbox/external_packages/python-mode.el-6.0.4/") 
+;; (setq py-install-directory "~/toolbox/external_packages/python-mode.el-6.0.4/")
+;; (require 'python-mode)
+
+;; attempt to get Emacs's "M-x compile" to work with the newly merged
+;; in "unification" branch, 2/23/2012
+(setenv "PYTHONPATH" (shell-command-to-string "source ~swain/.bashrc; source ~/.vpy/default/bin/activate; echo -n $PYTHONPATH"))
+
+;; Copied from python.el that came with Emacs 24.3.1
+;; (setq
+;;  python-shell-interpreter "ipython"
+;;  python-shell-interpreter-args ""
+;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;  python-shell-completion-setup-code
+;;    "from IPython.core.completerlib import module_completion"
+;;  python-shell-completion-module-string-code
+;;    "';'.join(module_completion('''%s'''))\n"
+;;  python-shell-completion-string-code
+;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;; 
+;; (load-file "~swain/.emacs.d/ipython.el")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; originally from ssc.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; insert-tabs-hook-func defined in ssc.el
+;; (add-hook 'python-mode-hook     'insert-tabs-hook-func)
+
+;; don't add trailing whitespace
+(add-hook 'python-mode-hook
+		  (lambda ()
+			(setq show-trailing-whitespace t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; originally from ~/.emacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (setq py-install-directory "~swain/.emacs.d/python-mode.el-6.2.0/")
+;; (add-to-list 'load-path py-install-directory)
+;; (require 'python-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; new stuff (based on instructions in python.el)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; obsolete in 25.2..?
+;(setq python-shell-virtualenv-path "~swain/.vpy/27production0/")
+
+(setq python-shell-virtualenv-root "~swain/.vpy/27production0/")
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i")
+
+;; in conjunction with JRR... guess what the indentation is
+(load-file "~swain/.emacs.d/guess-style-master/guess-style.el")
+(autoload 'guess-style-set-variable "guess-style" nil t)
+(autoload 'guess-style-guess-variable "guess-style")
+(autoload 'guess-style-guess-all "guess-style" nil t)
+(add-hook 'python-mode-hook 'guess-style-guess-all)
+
+
+;; Enable flycheck everywhere is a bad idea because we have a mountain
+;; of old code
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Dunno why but flycheck cannot find the executables, tho they are in
+;; my path.
+;; disable flake8 as we are tied to an older version which can't find mccabe
+;; (setq flycheck-python-flake8-executable
+;;       "/Users/swain/.vpy/27production0/bin/flake8")
+(setq flycheck-python-pylint-executable
+      "/Users/swain/.vpy/27production0/bin/pylint")
+
+;; wtf why don't these work? that's why I set two of them
+(setq python-check-command "pylint")
+(setq python-check-custom-command "pylint")
