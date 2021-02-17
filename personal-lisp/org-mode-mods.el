9bf5a3bd56d34ea6d4a5496f7dbd635c552510f9
Steve Wainstead <spawlowski@diligent.com>
2021-02-17 08:45:18 -0500

    tweaks for org-mode, updated .gitignore

--- personal-lisp/org-mode-mods.el
+++ personal-lisp/org-mode-mods.el
@@ -4,20 +4,10 @@
 ;;(setq org-default-notes-file "~/Dropbox/projects/GTD/notes.org")
 (define-key global-map "\C-cc" 'org-capture)
 
-;; org-mode uses this for highlighting a node (via org-mark-element);
-;; but I use it for a prefix key
-(define-key org-mode-map (kbd "M-a") nil)
-
-;; Move this; and put this stuff under version control please.
-;; Pilfered from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
-(setq org-agenda-files '("~/Documents/GTD/inbox.org"
-                         "~/Documents/GTD/gtd.org"
-                         "~/Documents/GTD/tickler.org"))
-
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
                                (file+headline "~/Documents/GTD/inbox.org" "Tasks")
                                "* TODO %i%?")
-                              ("T" "Tickler" entry
+                              ("i" "Tickler" entry
                                (file+headline "~/Documents/GTD/tickler.org" "Tickler")
                                "* %i%? \n %U")
                               ("w" "Waiting [inbox]" entry
@@ -28,11 +18,27 @@
                                "* %i%? \n %U")
 			      ))
 
+;; org-mode uses this for highlighting a node (via org-mark-element);
+;; but I use it for a prefix key
+(define-key org-mode-map (kbd "M-a") nil)
+;; org-mode uses this for highlighting the current heading
+(define-key org-mode-map (kbd "M-h") 'ns-do-hide-emacs)
+(define-key org-mode-map (kbd "A-<down>") 'refmin)
+;; Move this; and put this stuff under version control please.
+;; Pilfered from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
+(setq org-agenda-files '("~/Documents/GTD/inbox.org"
+                         "~/Documents/GTD/gtd.org"
+                         "~/Documents/GTD/tickler.org"
+						 "~/Documents/GTD/minutes.org"
+						 )
+	  )
+
 ;; For use in refiling tasks from inbox.org
 (setq org-refile-targets '(("~/Documents/GTD/gtd.org" :maxlevel . 3)
                            ("~/Documents/GTD/someday.org" :level . 1)
                            ("~/Documents/GTD/tickler.org" :maxlevel . 2)
-			   ("~/Documents/GTD/habits.org" :maxlevel . 2)
+						   ("~/Documents/GTD/minutes.org" :maxlevel . 2)
+						   ("~/Documents/GTD/habits.org" :maxlevel . 2)
 			   ))
 
 (setq org-todo-keywords '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)" "DEFERRED(r)" "OBSOLETE(o)")))
