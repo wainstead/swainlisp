b80123e99fe10afc1de1a61c0beb95c7a92ad50a
Steve Wainstead <swain@panix.com>
2021-05-14 07:34:00 -0400

    Manage list of load-files for abzug

--- external-packages/load-external-packages.el
+++ external-packages/load-external-packages.el
@@ -2,8 +2,8 @@
 ;; somewhere.
 (load-file "key-chord.el")
 (load-file "diff-mode-.el")
-(load-file "emmet-mode/emmet-mode.el")
-(load-file "prettier-emacs/prettier-js.el")
-(load-file "alfred-buffers.el")
+;;(load-file "emmet-mode/emmet-mode.el")
+;;(load-file "prettier-emacs/prettier-js.el")
+;;(load-file "alfred-buffers.el")
 (load-file "god-mode/god-mode.el")
-(load-file "alfred-buffers.el")
+
